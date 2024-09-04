13b5c4658020fd47ac0c668294c91c12529512b1
Trevor Mee <122403539+trevormee@users.noreply.github.com>
2024-09-04 21:03:45 +0000

    Password Logic - includes reading in names.txt - extracting userIds - generating random passwords - writing userid password combo to rawdata.txt

---
+++ src/Headers/Password.hpp
@@ -0,0 +1,45 @@
+/** @file Password.hpp
+ *  @brief Contains function declarations for reading in a text file,
+ *        generating random passwords, and writing the generated
+ *        generated passwords to a new text file
+ * 
+ * @author Trevor Mee
+*/
+
+#ifndef PASSWORD_HPP
+#define PASSWORD_HPP
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+
+class Password
+{
+    private:
+        std::string userId;
+        std::string password;
+        std::vector<std::vector<std::string>> userIdPasswords;
+
+    public:
+        const std::string NAMES_TEXT = "../names.txt";
+        const int PASSWORD_LENGTH = 9;
+        static const char alphabet[];
+
+        // Read in names.txt
+        void ReadFile();
+
+        // Generate random passwords
+        std::string GenerateRandomPasswords();
+
+        // Populates the userIdPasswords vector
+        void PopulateUserIdPasswords();
+
+        // Write Passwords to "rawdata.txt"
+        void WritePasswordsToFile();
+
+};
+#endif
