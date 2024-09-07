3ba55cd241cd6d3fd7351be6ba37fce7d8eea9d1
Trevor Mee <122403539+trevormee@users.noreply.github.com>
2024-09-07 17:30:48 +0000

    Finished Part 1 of Project 1 - Added Cipher.hpp & Cipher.cpp - Updated makefile - Implemented functions to... -- Read in rawdata.txt -- create a forward & reverse alphabet hash map -- encrypt function that implements vigenere cipher logic -- function to populate vector with encryped passwords -- function to write encrypted pw and user id pair to new file "encrypteddata.txt"

---
+++ src/Cipher.cpp
@@ -0,0 +1,131 @@
+#include "Headers/Cipher.hpp"
+
+/*
+    @brief reads in rawdata.txt and pushes the user id
+           and plaintext pw to a vector
+*/
+void Cipher::ReadFile()
+{
+    std::ifstream fileRead(RAW_DATA_TEXT);
+    std::string line;
+
+    while(std::getline(fileRead, line))
+    {
+        std::istringstream iss(line);
+        iss >> userId >> plaintextPw;
+        userIdEncryptedPasswords.push_back({userId, plaintextPw});
+       //std::cout << "UserId:" << userId << " pw: " << plaintextPw << std::endl;
+    }
+    std::cout << std::endl;
+
+    fileRead.close();
+}
+
+/*
+    @brief helper function for Encrypt() to create a 
+           alphabet hash map for vigenere cipher logic
+    @returns hash map of the alphabet
+*/
+std::unordered_map<char, int> Cipher::CreateAlphabetMap()
+{
+    //std::cout << "Alphabet size -> "<< alphabet.size() << std::endl;
+    for(int i = 0; i < alphabet.size(); ++i)
+    {
+        alphabetMap.insert({alphabet[i], i + 1});
+    }
+/*
+    for (const auto& pair : alphabetMap)
+    {
+        std::cout << "Key: " << pair.first << ", Value: " << pair.second << std::endl;
+    }
+*/
+    return alphabetMap;
+
+}
+
+/*
+    @brief helper function for Encrypt() to create a 
+           reversed alphabet hash map
+    @returns reversed alphabet hash map
+*/
+std::unordered_map<int, char> Cipher::CreateReverseAlphabetMap()
+{
+    for(int i = 0; i < alphabet.size(); ++i)
+    {
+        reverseAlphabetMap.insert({i + 1, alphabet[i]});
+    }
+/*
+    for (const auto& pair : reverseAlphabetMap)
+    {
+        std::cout << "Key: " << pair.first << ", Value: " << pair.second << std::endl;
+    }
+*/
+    return reverseAlphabetMap;
+}
+
+/*
+    @brief encrypts the plaintext passwords
+           Math: C_i = (P_i + K_i) mod 26 
+    @param plain text password we are looking to encrypt
+    @returns encrypted password
+*/
+std::string Cipher::Encrypt(std::string plaintextPassword)
+{
+    std::cout << "Plaintxt Pw:  " << plaintextPassword << std::endl;
+
+    encryptedPw.clear();
+    CreateAlphabetMap();
+    CreateReverseAlphabetMap();
+    for(int i = 0; i < plaintextPassword.size(); ++i)
+    {
+        char plainTextCurrLetter = plaintextPassword[i];
+        char keyCurrLetter = KEY[i];
+        if(alphabetMap.find(plainTextCurrLetter) != alphabetMap.end()){
+            
+            int plainTextLetterToInt = alphabetMap[plainTextCurrLetter];
+            int keyToInt = alphabetMap[keyCurrLetter];
+            int encryptedLetterToInt = (plainTextLetterToInt + keyToInt) % alphabet.size();
+
+            if(reverseAlphabetMap.find(encryptedLetterToInt) != reverseAlphabetMap.end())
+            {
+                encryptedPw += reverseAlphabetMap[encryptedLetterToInt];
+            }
+        }
+    }
+    std::cout << "Encrypted Pw:  " << encryptedPw << std::endl;
+    return encryptedPw;
+    
+}
+
+/*
+    @brief Populates the 2D vector 'userIdEncrypedPasswords' with
+           encryped passwords 
+*/
+void Cipher::PopulateUserIdEncryptedPasswords()
+{
+    //ReadFile();
+
+    for(long unsigned int i = 0; i < userIdEncryptedPasswords.size(); ++i)
+    {
+        userIdEncryptedPasswords[i][1] = Encrypt(userIdEncryptedPasswords[i][1]);
+    }
+}
+
+/*
+    @brief writes user ids and encryped data to "encrypeddata.txt"
+*/
+void Cipher::WriteEncryptedPasswordsToFile()
+{
+    ReadFile();
+    PopulateUserIdEncryptedPasswords();
+
+    std::ofstream outputFile;
+    outputFile.open(ENCRYPTED_DATA_TEXT);
+
+    for(long unsigned int i = 0; i < userIdEncryptedPasswords.size(); ++i)
+    {
+        outputFile << userIdEncryptedPasswords[i][0] << ", " << userIdEncryptedPasswords[i][1] << "\n";
+    }
+
+    outputFile.close();
+}
---
+++ src/Headers/Cipher.hpp
@@ -0,0 +1,54 @@
+/** @file Cipher.hpp
+ *  @brief Contains function declarations for reading in a text file,
+ *         encrypting user passwords using the Vigenere Cipher, and 
+ *         writing the encrpyted passwords to a new text file
+ * 
+ * @author Trevor Mee
+*/
+
+
+#ifndef CIPHER_HPP
+#define CIPHER_HPP
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+
+class Cipher
+{
+    private:
+        const std::string KEY = "microsoft";
+        const std::string ENCRYPTED_DATA_TEXT = "../encrypteddata.txt";
+        const std::string RAW_DATA_TEXT = "../rawdata.txt";
+        std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        std::vector<std::vector<std::string>> userIdEncryptedPasswords;
+        std::string userId;
+        std::string plaintextPw;
+        std::string encryptedPw = "";
+
+    public:
+        std::unordered_map<char, int> alphabetMap;
+        std::unordered_map<int, char> reverseAlphabetMap;
+
+        // Reads in rawdata.txt
+        void ReadFile();
+
+        // Creates a hash map of the alphabet
+        std::unordered_map<char, int> CreateAlphabetMap();
+
+        // Creates a reversed alphabet map for encrypting passwords
+        std::unordered_map<int, char> CreateReverseAlphabetMap();
+
+        // Encrpyts the passwords using vigenere cipher logic
+        std::string Encrypt(std::string plaintextPassword);
+        
+        // Populates a 2D vector with user id's and encrypted passwords
+        void PopulateUserIdEncryptedPasswords();
+        
+        // Write encrypted passwords to "encrpyteddata.txt"
+        void WriteEncryptedPasswordsToFile();
+};
+#endif
